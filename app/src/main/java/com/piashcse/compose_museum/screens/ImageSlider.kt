4df283487353e4bf484447556f4bff7bb0b3e85a
Mehedi Hassan Piash <piash599@gmail.com>
2024-11-08 22:20:30 +0600

    - Update slider image url - Fixing bottomsheet background color

--- app/src/main/java/com/piashcse/compose_museum/screens/ImageSlider.kt
+++ app/src/main/java/com/piashcse/compose_museum/screens/ImageSlider.kt
@@ -13,11 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,39 +28,37 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberAsyncImagePainter
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.yield
 
 @Composable
 fun ImageSlider(navController: NavController) {
     val images = listOf(
-        "https://picsum.photos/id/1/200/300" to "First image ",
-        "https://picsum.photos/id/1/200/300" to "Second image",
-        "https://picsum.photos/id/1/200/300" to "Third image"
+        "https://image.tmdb.org/t/p/w500//j736cRzBtEPCm0nHnpRN1prqiqj.jpg" to "First Image",
+        "https://image.tmdb.org/t/p/w500/3EpZ2ksjijmdr8BhISP03PYzNFW.jpg" to "Second Image",
+        "https://image.tmdb.org/t/p/w500/flSncTaSISRqrqoJ18ZBmThR4Ee.jpg" to "Third Image"
     )
     ImageSlider(images = images, modifier = Modifier.fillMaxSize())
 }
 
-
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ImageSlider(images: List<Pair<String, String>>, modifier: Modifier = Modifier) {
-    val pagerState = rememberPagerState() {
+    val pagerState = rememberPagerState {
         3
     }
     val coroutineScope = rememberCoroutineScope()
 
-    LaunchedEffect(Unit) {
+    LaunchedEffect(pagerState) {
         while (true) {
-            yield() // Required to allow Compose to do other work
             delay(3000) // Delay for auto-scrolling
             coroutineScope.launch {
                 pagerState.animateScrollToPage(
-                    page = (pagerState.currentPage + 1) % images.size
+                    (pagerState.currentPage + 1) % images.size
                 )
             }
         }
@@ -76,50 +72,28 @@ fun ImageSlider(images: List<Pair<String, String>>, modifier: Modifier = Modifie
             Box(modifier = Modifier.fillMaxSize()) {
                 Image(
                     painter = rememberAsyncImagePainter(images[page].first),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop, // Center crop the image
+                    contentDescription = images[page].second,
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier.fillMaxSize()
                 )
                 Text(
                     text = images[page].second,
+                    fontSize = 12.sp,
                     color = Color.White,
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
                         .padding(8.dp)
-                        .background(Color.Black.copy(alpha = 0.2f))
-                        .padding(8.dp)
+                        .background(Color.Black.copy(alpha = 0.2f), shape = RoundedCornerShape(18))
+                        .padding(start = 6.dp, end = 6.dp)
                 )
             }
-
         }
-        FlatIndicators(size = images.size, index = pagerState.currentPage)
+        IndicatorRow(size = images.size, index = pagerState.currentPage)
     }
 }
 
 @Composable
-fun Indicators(size: Int, index: Int) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        repeat(size) { i ->
-            Box(
-                modifier = Modifier
-                    .size(if (i == index) 12.dp else 8.dp)
-                    .background(
-                        if (i == index) Color.Blue else Color.Gray,
-                        shape = CircleShape
-                    )
-                    .padding(4.dp)
-            )
-        }
-    }
-}
-
-@Composable
-fun FlatIndicators(size: Int, index: Int) {
+fun IndicatorRow(size: Int, index: Int) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -145,7 +119,7 @@ fun FlatIndicators(size: Int, index: Int) {
 
 @Preview(showBackground = true)
 @Composable
-fun ImageSliderPreview(){
+fun ImageSliderPreview() {
     val navController = rememberNavController()
     ImageSlider(navController)
 }
